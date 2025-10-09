eea9e1f3455a72a88abd1e56c648d6f442329252
MowlandCodes <mowlandgaming@gmail.com>
2025-10-10 00:00:47 +0700

    update callback

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from uuid import uuid4
 
 import requests
 from dotenv import load_dotenv
-from flask import Flask, redirect, render_template, request, session, url_for
+from flask import (Flask, json, redirect, render_template, request, session,
+                   url_for)
 
 # Load env file
 load_dotenv()
@@ -33,6 +34,33 @@ def index():
 def login():
     authUrl = f"{SSO_BASE_URL}/oauth/login?client_id={SSO_CLIENT_ID}&redirect_uri={SSO_REDIRECT_URI}&response_type=code&scope=read write&state={uuid4()}"
     return redirect(authUrl)
+
+
+def callback():
+    # Auth Code dari SSO Callback
+    authCode = request.args.get("code")
+
+    if not authCode:
+        return render_template("error.html", message="Auth Code Tidak Ditemukan")
+
+    tokenPayload = {
+        "client_id": SSO_CLIENT_ID,
+        "client_secret": SSO_CLIENT_SECRET,
+        "grant_type": "authorization_code",
+        "code": authCode,
+        "redirect_uri": SSO_REDIRECT_URI,
+    }
+
+    try:
+        tokenResponse = requests.post(f"{SSO_BASE_URL}/oauth/token", data=tokenPayload)
+        tokenResponse.raise_for_status()
+
+        tokenData = tokenResponse.json()
+        token = tokenData["access_token"]
+
+        return json.jsonify(token)
+    except Exception as e:
+        raise e
 
 
 # Run app
